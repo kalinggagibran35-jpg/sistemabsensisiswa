01a290bda1cfc7c92103eac6b861630276707966
kalinggagibran35-jpg <kalinggagibran35@gmail.com>
2026-06-11 16:02:02 +0800

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: ReturnType<typeof makePrismaClient> | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+function makePrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+export const db = globalForPrisma.prisma ?? makePrismaClient()
+
+globalForPrisma.prisma = db
